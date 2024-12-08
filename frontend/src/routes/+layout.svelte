0f52d1d95a1a55a19040525b9a45fdaf63cdb71e
eske <eske.brun3@gmail.com>
2024-12-08 11:13:25 +0100

    svelte

---
+++ frontend/src/routes/+layout.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+const {children, data} = $props();
+</script>
+
+<header>
+<nav>
+<ul>
+{#each data.pages as page}
+<li>
+<a href="{page.href}">{page.name}</a>
+</li>
+</ul>
+</nav>
+{/each}
+
+</header>
+<main>
+{@render children()}
+</main>
